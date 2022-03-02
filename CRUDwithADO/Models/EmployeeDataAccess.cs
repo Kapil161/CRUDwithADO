6482101aa52122984535371aab669b4fb182e929
kapil161 <tyagikapil914@gmail.com>
2022-03-02 10:06:31 +0530

    Add project files.

---
+++ CRUDwithADO/Models/EmployeeDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+
+namespace CRUDwithADO.Models
+{
+    public class EmployeeDataAccess
+    {
+        DBConnection DBConnection;
+        public EmployeeDataAccess()
+        {
+            DBConnection = new DBConnection();
+        }
+        public List<Employees> GetEmployees()
+        {
+            string Sp = "SP_EMPLOYEE";
+            SqlCommand sql = new SqlCommand(Sp, DBConnection.Connection);
+            sql.CommandType = CommandType.StoredProcedure;
+            sql.Parameters.AddWithValue("@action", "SELECT_JOIN");
+            if (DBConnection.Connection.State == ConnectionState.Closed)
+            {
+                DBConnection.Connection.Open();
+            }
+            SqlDataReader dr = sql.ExecuteReader();
+            List<Employees> employees = new List<Employees>();
+            while(dr.Read())
+            {
+                Employees Emp = new Employees();
+                Emp.Id = (int)dr["id"];
+                Emp.Name = dr["name"].ToString();
+                Emp.Email = dr["email"].ToString();
+                Emp.Mobile = dr["mobile"].ToString();
+                Emp.Gender= dr["gender"].ToString();
+                Emp.DName = dr["department"].ToString();
+                employees.Add(Emp);
+            }
+            DBConnection.Connection.Close();
+            return employees;
+        }
+        
+    }
+}
---
+++ CRUDwithADO/Models/Employees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDwithADO.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Email{ get; set; }
+        public string Mobile { get; set; }
+        public string Gender{ get; set; }
+        public int DeptId { get; set; }
+        public string DName { get; set; }
+    }
+}
